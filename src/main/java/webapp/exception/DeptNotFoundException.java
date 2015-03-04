bed2afcb3ca461754bc9da2d4f2c9ee9978d8631
teacher <protocls@nate.com>
2015-03-04 15:41:00 +0900

    webapp package add

---
+++ src/main/java/webapp/exception/DeptNotFoundException.java
@@ -0,0 +1,17 @@
+package webapp.exception;
+
+public class DeptNotFoundException extends RuntimeException {
+	
+	public DeptNotFoundException() {
+		super();
+	}
+	
+	public DeptNotFoundException(String message) {
+		super(message);
+	}
+	
+	public DeptNotFoundException(String message, Throwable e) {
+		super(message, e);
+	}
+
+}
